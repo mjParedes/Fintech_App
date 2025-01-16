5b38558102b222d571a58a81ce1335567e59d269
Raydberg Gabriel Chuquival Gil <raydga_2005@hotmail.com>
2025-01-16 11:14:29 -0500

    fix: adding filtering methods for all entity Instrument , Portfolio , Transaction

--- server/src/main/java/com/practice/Notification/dtoRequest/NotificationRequestDto.java
+++ server/src/main/java/com/practice/Notification/dtoRequest/NotificationRequestDto.java
@@ -1,11 +1,16 @@
 package com.practice.Notification.dtoRequest;
 
-import jakarta.validation.constraints.NotBlank;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-import java.util.List;
-
-public record NotificationRequestDto(
-        @NotBlank(message = "El tipo de notificacion es obligatorio") List<String>  type_notification,
-        @NotBlank(message = "El mensaje es obligatorio") String message,
-        @NotBlank(message = "Campo leido marcar correctamente") Boolean isRead) {
+@Data
+@NoArgsConstructor
+@Schema(description = "")
+public class NotificationRequestDto {
+    private Long id;
+    private String typeNotification;
+    private String message;
+    private Boolean isRead;
+    private Long userId;
 }
--- server/src/main/java/com/practice/Portfolio/service/PortfolioService.java
+++ server/src/main/java/com/practice/Portfolio/service/PortfolioService.java
@@ -1,4 +1,7 @@
 package com.practice.Portfolio.service;
 
+import com.practice.Portfolio.dtoResponse.PortfolioPageResponseDto;
+
 public interface PortfolioService {
+    PortfolioPageResponseDto findAllPortfolio(int page, int size);
 }
