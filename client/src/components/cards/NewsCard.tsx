ee4d21bc405d56ca174b0dde5248a543670e44c7
Bárbara Espinola <Baesp2023@gmail.com>
2025-01-30 00:22:58 -0300

    Community News + Estudent

---
+++ client/src/components/cards/NewsCard.tsx
@@ -0,0 +1,50 @@
+import { CarsNews, CriptosNews, GlobalNews, MedicineNews, UserPhotoDefault } from "@/assets";
+import { useNewsStore } from "@/store/community/news";
+import ChatBubbleOutlineOutlinedIcon from '@mui/icons-material/ChatBubbleOutlineOutlined';
+import BookmarkBorderOutlinedIcon from '@mui/icons-material/BookmarkBorderOutlined';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import Image from "next/image";
+export const NewsSection = () => {
+
+type ImgType = 'Criptos' | 'Medicine' | 'Cars' | "Global"
+
+const NewsMap: Record<ImgType, string> = {
+  Criptos: CriptosNews, 
+  Medicine: MedicineNews,
+  Cars: CarsNews,
+  Global: GlobalNews
+};
+  const { items } = useNewsStore();
+
+  return (
+    <div >
+        <h1 className="text-h5-semibold text-white900 text-center pb-2">Noticias de Finanzas</h1>
+      <div className=" flex flex-col">
+        {Object.entries(items).map(([id, { img, title, message, autor, date, comments }]) => (
+          <div key={id}>
+            <Image src={NewsMap[img as ImgType] }alt={title} className="w-full h-40 object-cover rounded-lg mb-4" />
+          <div className="p-4">
+            <div className= "flex flex-row text-p1-medium gap-2 text-white600 items-center">
+                <Image src={UserPhotoDefault} alt="" className="w-4 h-4 "/>{autor + " · "} 
+                <p className="text-p1-semibold">{date}</p>
+            </div>
+            <h3 className="text-white900 text-h5-semibold p-1">{title}</h3>
+            <p className="text-white600 text-p1-regular p-1">{message}</p>
+
+            <div className="flex flex-row justify-between p-1">
+                <div className=" flex flex-row gap-2">
+                    <ChatBubbleOutlineOutlinedIcon/>
+                    {comments}
+                </div>
+                <div className=" flex flex-row gap-2">
+                    <BookmarkBorderOutlinedIcon/>
+                    <MoreHorizIcon/>
+                </div>
+            </div>
+          </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
---
+++ client/src/components/slider/community.tsx
@@ -0,0 +1,58 @@
+import {  educationActive, educationDefault, newsActive, newsDefault} from "@/assets"
+import Image from "next/image"
+import { useState } from "react"
+import { CarouselComunnity } from "../carousel/communityUser"
+import { Search } from "@mui/icons-material"
+import { NewsSection } from "../cards/NewsCard"
+
+export const SliderCommunity = () =>{
+    
+    const [visualData , setVisualData] = useState("Education")
+
+    const handleChangeEducation = () =>{
+        setVisualData("Education")
+    }
+
+    const handleChangeNews = () =>{
+        setVisualData("News")
+    }
+
+    return (
+    <div className="w-full flex flex-col gap-4 mx-auto h-full">
+
+        <div className="flex flex-row justify-around gap-1 p-2">
+            <button onClick={handleChangeEducation}>
+                { visualData === "Education" 
+                ? <Image src={educationActive} alt=""/>
+                : <Image src={educationDefault} alt=""/>
+                }
+            </button>
+
+            <button onClick={handleChangeNews}>
+                { visualData === "News"
+                ? <Image src={newsActive} alt=""/>
+                : <Image src={newsDefault} alt=""/>
+                }
+            </button>
+        </div>
+
+
+        { visualData === "Education" ?
+        <>        
+            <div className="flex flex-row gap-2 border w-[23.5em] p-1 border-white300 rounded items-center mx-auto">
+                <Search className=" ml-2 opacity-50"/>
+                <input
+                type="text"
+                placeholder="Buscar por temas "
+                className=" text-p2-regular text-white600">
+                </input>
+            </div>
+            <CarouselComunnity />
+        </>
+        : <NewsSection/>
+        }
+        
+
+
+    </div>)
+}
